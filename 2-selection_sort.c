fc0806a44f4fa69113ec4221cd06eaaf9ec0c35a
DilanSoto <dilanso12@gmail.com>
2024-01-18 20:18:43 -0400

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,17 +9,20 @@
 */
 
 void selection_sort(int *array, size_t size)
-{
-size_t i, j, min;
-int temp;
 
-for (i = 0; i < size - 1; i++)
 {
-	min = i;
-	for (j = i + 1; j < size; j++)
+	size_t i, j, min;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
 	{
-		if (array[j] < array[min])
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
 				min = j;
+			}
 		}
 		if (min != i)
 		{
